3782411c49c815bc7bbfd0ff291f627eb309e87d
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:39:38 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/policymanager/trunk@571 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/policymanager.js
@@ -0,0 +1,2 @@
+pref("extensions.{CE494EE3-65A1-4f63-8FC1-645217F86979}.name", "chrome://policymanager/locale/policymanager.properties");
+pref("extensions.{CE494EE3-65A1-4f63-8FC1-645217F86979}.description", "chrome://policymanager/locale/policymanager.properties");
